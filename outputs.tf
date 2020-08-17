88430155608cba90b8617b8eb5811bf4da8a1f40
Paolo Cadiz <pscadiz@quadx.xyz>
2020-08-17 20:56:00 +0800

    Revert "added fargate_profile_iam_role_arn output"
    This reverts commit 12e6e2b0ed783a44b43017c9a9c25f079214ffae.

--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,3 @@ output "cluster_security_group_id" {
   description = "Security Group ID used by the cluster"
   value       = aws_eks_cluster.eks_cluster.vpc_config.0.cluster_security_group_id
 }
-
-output "fargate_profile_iam_role_arn" {
-  description = "ARN of the IAM role attached to the fargate profiles"
-  value = aws_iam_role.iam_role_fargate_profile.arn
-}
