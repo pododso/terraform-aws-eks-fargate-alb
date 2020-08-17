32f910f2ed72d7036a60c340a9b4b1920f57e351
Paolo Cadiz <pscadiz@quadx.xyz>
2020-08-17 21:18:52 +0800

    added oidc url and arn output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "cluster_security_group_id" {
   description = "Security Group ID used by the cluster"
   value       = aws_eks_cluster.eks_cluster.vpc_config.0.cluster_security_group_id
 }
+
+output "oidc_provider_arn" {
+  description = "OpenID Connect Provider ARN of the EKS Cluster"
+  value       = aws_iam_openid_connect_provider.iam_oidc_provider.arn
+}
+
+output "oidc_provicer_url" {
+  description = "OpenID Connect Provider URL of the EKS Cluster"
+  value       = aws_iam_openid_connect_provider.iam_oidc_provider.url
+}
