12e6e2b0ed783a44b43017c9a9c25f079214ffae
Paolo Cadiz <pscadiz@quadx.xyz>
2020-08-17 18:45:05 +0800

    added fargate_profile_iam_role_arn output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "cluster_security_group_id" {
   description = "Security Group ID used by the cluster"
   value       = aws_eks_cluster.eks_cluster.vpc_config.0.cluster_security_group_id
 }
+
+output "fargate_profile_iam_role_arn" {
+  description = "ARN of the IAM role attached to the fargate profiles"
+  value = aws_iam_role.iam_role_fargate_profile.arn
+}
